b3a7e4386cf054417f81ee6d27b636f2a4a3fa09
Jordan <personal.jordanabraham@gmail.com>
2023-04-25 22:39:59 -0400

    Add basic decompiler and basic test.

--- decompiler/src/main/kotlin/com/shockwave/decompiler/Decompiler.kt
+++ decompiler/src/main/kotlin/com/shockwave/decompiler/Decompiler.kt
@@ -1,6 +1,17 @@
 package com.shockwave.decompiler
 
+import java.io.File
+import java.nio.ByteBuffer
+
 /**
  * @author Jordan Abraham
  */
-class Decompiler {}
+class Decompiler(
+    private val resource: File
+) {
+    fun decompile() {
+        // Allows read of file resource up to 2GB.
+        val buffer = ByteBuffer.wrap(resource.readBytes())
+        println(buffer.capacity())
+    }
+}
--- decompiler/src/test/kotlin/com/shockwave/decompiler/DecompilerTest.kt
+++ decompiler/src/test/kotlin/com/shockwave/decompiler/DecompilerTest.kt
@@ -1,5 +1,7 @@
 package com.shockwave.decompiler
 
+import java.io.File
+import java.lang.IllegalStateException
 import kotlin.test.Test
 
 /**
@@ -9,6 +11,9 @@ class DecompilerTest {
 
     @Test
     fun `test decompile hh_badges cct`() {
-        // TODO
+        val resource = this::class.java.classLoader.getResource("hh_badges.cct") ?: throw IllegalStateException("Resource file not found.")
+        val cctFile = File(resource.toURI())
+        val decompiler = Decompiler(cctFile)
+        decompiler.decompile()
     }
 }
